e366badf40fe0d90acad093c1e055a959b69f019
georginacyc <georginaschoolstuff@gmail.com>
2021-11-30 23:12:46 +0800

    successfully connected flask app to rds. can query and display records from table. next step is search function.

--- Georgina/app.py
+++ Georgina/app.py
@@ -1,19 +1,40 @@
 from flask import Flask, config, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 import boto3
 from botocore.config import Config
 
 app = Flask(__name__)
 
-# app_config = Config(
-#     region_name = 'us-east-1'
-# )
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:password@scientia.cecynww2bmvp.us-east-1.rds.amazonaws.com/scientiaTest'
+db = SQLAlchemy(app)
 
-# db_data = boto3.client('db_data', config=app_config, aws_access_key_id='TODO', aws_secret_access_key='TODO')
+# basically disabling logs, so no bloating
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db_name = 'TODO'
-# db_cluster_arn = 'TODO'
-# db_secret_arn = 'TODO'
+migrate = Migrate(app, db)
+
+class forum_posts(db.Model):
+    __tablename__ = "forum_posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    forum = db.Column(db.String())
+    type = db.Column(db.String())
+    title = db.Column(db.String())
+    body = db.Column(db.String())
+    author = db.Column(db.String())
+
+    def __init__(self, forum: str, type: str, title: str, body: str, author: str):
+        self.forum = forum
+        self.type = type
+        self.title = title
+        self.body = body
+        self.author = author
+
+    # how the data is represented. this affects how the data will be returned on a query. MUST BE STRING
+    # def __repr__(self):
+    #     return f"{self.forum},{self.type},{self.title},{self.body},{self.author}"
 
 @app.route('/')
 def home():
@@ -21,7 +42,10 @@ def home():
 
 @app.route('/forum')
 def getPosts():
-    return render_template("forum_posts.html")
+    # return render_template("forum_posts.html")
+    posts = forum_posts.query.all()
+    # return '\n'.join([str(post) for post in posts])
+    return render_template("forum_posts.html", posts=posts)
 
 if __name__ == '__main__':
     app.run(threaded=True, host='0.0.0.0', port=8080)
